ec4accb2675c42564b2e426e54ab3565bb6a6350
Ouail Aouattah <ouail@onyxstudios.onmicrosoft.com>
2024-02-17 19:04:08 +0100

    mejorando poco a poco

--- dabo.py
+++ dabo.py
@@ -1,1 +1,6 @@
-("")
+age = 25
+price = 20.30
+first_name = 'Ouail'
+is_online = True
+print(age)
+print('Jabo de "Dabo Dalango"')
---
+++ type.py
@@ -0,0 +1,5 @@
+birth_year = input('Enter your birth year: ')
+age = 2024 - int(birth_year)
+print(age)
+
+float()
